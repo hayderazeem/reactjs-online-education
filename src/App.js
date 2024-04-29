badf0becd6944cea85a3083c8644c30ea9da6ea2
Azeem Haider <hayder.azeem@gmail.com>
2024-04-29 23:47:43 +0500

    Setup Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Layout from "./layout";
+import HomePage from "./pages/homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <HomePage />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
