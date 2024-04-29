2eda12a77e1b2a3454011abb5b9858cd9f138f22
Azeem Haider <hayder.azeem@gmail.com>
2024-04-30 00:02:11 +0500

    Footer Update

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,17 +1,56 @@
 import React from 'react'
-
+import Logo from '../assets/logo.png'
+import "../layout/footer.css"
 const Footer = () => {
     return (
         <footer>
             <div className="page_width">
                 <div className="inner_footer">
-                    <div className>1</div>
-                    <div className>2</div>
-                    <div className>3</div>
-                    <div className>4</div>
-                    <div className>5</div>
-                </div>
-            </div>
+                <div className="logo"><img src={Logo} alt="" />
+        <p className="footer_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy a type specimen book.</p>
+      </div>
+      <div className="footer_c1">
+        <h2>Company</h2>
+        <ul>
+          <li>About Us</li>
+          <li>How to work?</li>
+          <li>Populer Course</li>
+          <li>Service</li>
+        </ul>
+      </div>
+      <div className="footer_c1">
+        <h2>Courses</h2>
+        <ul>
+          <li>Categories</li>
+          <li>Ofline Course</li>
+          <li>Vidio Course</li>
+        </ul>
+      </div>
+      <div className="footer_c1">
+        <h2>Support</h2>
+        <ul>
+          <li>FAQ</li>
+          <li>Help Center</li>
+          <li>Career</li>
+          <li>Privacy</li>
+        </ul>
+      </div>
+      <div className="footer_c5">
+        <h2>Contact Info</h2><br />
+        <ul>
+          <li>+0913-705-3875</li><br />
+          <li>ElizabethJ@jourrapide.com</li>
+        </ul><br />
+        <p>4808 Skinner Hollow Road </p>
+        <p> Creek, OR 97429</p>
+      </div>
+    </div>
+  </div>
+  <br /><br />
+  <hr />
+  <p className="footer_copyright">BookStore All Right Reserved, 2022</p>
+              
         </footer>
     )
 }
