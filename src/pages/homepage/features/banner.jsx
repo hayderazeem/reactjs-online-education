c7a470dcb902d0f2bf3da9c4fef64a7d40e4c2a7
Azeem Haider <hayder.azeem@gmail.com>
2024-04-30 23:13:48 +0500

    Add Banner

--- src/pages/homepage/features/banner.jsx
+++ src/pages/homepage/features/banner.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import BannerImg from '../../../assets/banner_img.png'
+import "../features/banner.css"
+import BannerImg from '../../../assets/banner_img1.png'
+import Certification from '../../../assets/certification.png'
+import Exam from '../../../assets/exam.png'
+import OnlineTest from '../../../assets/online-test.png'
 
 const Banner = () => {
     return (
@@ -10,18 +14,67 @@ const Banner = () => {
                         <h2>
                             The <span>Smart</span> <br />
                             Choice For <span>Future</span>
-                        </h2>
+                        </h2><br />
                         <p>
-                            Elearn is a global training provider based across the UK that specialises in accredited and bespoke training courses. We crush the...
-                        </p>
+                            Elearn is a global training provider based across the UK that <br />specialises in accredited and bespoke training courses. We crush<br /> the...
+                        </p><br /><br />
                         <div className="search_box">
                             <input type="text" placeholder='Search for a location...' className='search_box_input' />
-                            <button>Continue</button>
+                            <input className="search_box_btn" type="button" value="Continue"/>
+                           
                         </div>
                     </div>
                     <div className="img">
                         <img src={BannerImg} width="100%" alt="" />
                     </div>
+
+
+
+
+
+
+                    <div className="bottom_banner">
+  <div className="grid-container">
+    <div className="grid-item item1">
+      <div className="img_box">
+        <img src={OnlineTest} alt="" width="34px" height="34px" />
+      </div>
+      <div>
+        <h3>Learn The Latest Skills</h3><br />
+        <p className="grid_p">Contrary to popular belief, Lorem Ipsum is <br />not simply random text. It has
+          roots in a <br />BC, making it over 2000 years old.</p>
+      </div>
+    </div>
+    <div className="grid-item item2">
+      <div className="img_box"><img src={Exam} alt="" width="34px" height="34px" />
+      </div>
+      <div>
+        <h3>Get Ready For a Career</h3><br />
+        <p className="grid_p">Contrary to popular belief, Lorem Ipsum is <br />not simply random text. It has
+          roots in
+          a <br />BC, making it over 2000 years old.</p>
+      </div>
+    </div>
+    <div className="grid-item item3">
+      <div className="img_box">
+        <img src={Certification} alt="" width="34px" height="34px" />
+      </div>
+      <div>
+        <h3>Get Ready For a Career</h3><br />
+        <p className="grid_p">Contrary to popular belief, Lorem Ipsum is <br />not simply random text. It has
+          roots in
+          a <br />BC, making it over 2000 years old.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
                 </div>
             </div>
         </div>
